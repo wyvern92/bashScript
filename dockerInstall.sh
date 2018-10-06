4c57652a0f275d2393efb9cdb9992f372bbb73c2
root <i271019477@qq.com>
2018-10-06 07:53:00 +0000

    docker install script init

---
+++ dockerInstall.sh
@@ -0,0 +1,12 @@
+ f# author : Yiwen Wang
+# date : 10/5/2018
+# description: this is a bash script for installing docker
+
+tput bold
+tput setaf 3
+echo @@@@@@@@@@@@@@--WAYNE LABORATORY PRODUCTION--@@@@@@@@@@@@@@
+echo @ the program for installing docker environment-------
+echo @ last modified time: 10/5/2018
+tput setaf 1
+echo --------- this program will start in 3 seconds ------------
+sleep 3s # Waits 3 seconds.
