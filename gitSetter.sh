a97c871f40021557f4ec043f29d730967483af2d
Yiwen <i271019477@qq.com>
2018-10-20 09:10:57 -0700

    add logo

---
+++ gitSetter.sh
@@ -0,0 +1,15 @@
+# author : Yiwen Wang
+# date : 10/20/2018
+# description: this is a bash script for installing node
+
+tput bold
+tput setaf 3
+echo @@@@@@@@@@@@@@--WAYNE LABORATORY PRODUCTION--@@@@@@@@@@@@@@
+echo @ the program for setting new git environment-------
+echo @ last modified time: 10/5/2018
+tput setaf 1
+echo --------- this program will start in 3 seconds ------------
+sleep 3s # Waits 3 seconds.
+
+git config --global user.name "wyvern92"
+git config --global user.email "i271019477@qq.com"
