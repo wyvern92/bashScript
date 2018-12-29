bc87d2e226ce945968ce09b660ab181fae4a408f
Yiwen <i271019477@qq.com>
2018-12-29 12:04:24 -0800

    Update rubyInstall.sh

--- rubyInstall.sh
+++ rubyInstall.sh
@@ -1,8 +1,19 @@
 sudo apt update
 sudo apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+
+# install rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# also install RVM, but use it as our default version control
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+curl -sSL https://get.rvm.io | bash -s stable --rails
+
+# our script is default to be run on ubuntu
+
+
 
