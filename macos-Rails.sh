c4b9f630fa0ff68e344facdfe94b6c830200ecf3
Yiwen <i271019477@qq.com>
2019-01-10 18:48:19 -0800

    Update macos-Rails.sh

--- macos-Rails.sh
+++ macos-Rails.sh
@@ -2,4 +2,9 @@ brew install selenium
 
 brew install chromedriver
 brew services start chromedriver
-Capybara.default_driver = :selenium_chrome
+# Capybara.default_driver = :selenium_chrome
+
+brew install redis
+ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
+
+
